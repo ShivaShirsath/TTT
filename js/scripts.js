08f6b51a63e3048c057cbed8f18bde5ac3d7612e
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-06-18 20:21:37 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,43 @@
-o="&#11093";
-x="&#10060";
-s="";
-turn=true;
-steps=0;
-document.onclick = function(e){
-    e.target.disabled=true;
-    //alert(e.target.id)
-    if(turn){
-        s=o;
-    } else {
-        s=x;
+o = document.getElementById("o");
+o.innerHTML = "&#11093";
+x = document.getElementById("x");
+x.innerHTML = "&#10060";
+
+turns = document.getElementById("turns");
+turns.innerHTML = x.innerHTML;
+t = "";
+s = "";
+turn = true;
+steps = 0;
+document.onclick = function(e) {
+  if (e.target.id=="turn") {
+    if (turns.innerHTML.includes(o.innerHTML)) {
+      s = o.innerHTML;
+      t = x.innerHTML;
+    } 
+    if (turns.innerHTML.includes(x.innerHTML)) {
+      s = x.innerHTML;
+      t = o.innerHTML;
     }
-    e.target.innerHTML=s;
-    turn =! turn;
+  }
+  if (e.target.type == "submit" && e.target.innerHTML == "") {
+    e.target.disabled = true;
+    document.getElementById("turn").disabled = true;
+    
+    turns.innerHTML = t;
+    e.target.innerHTML = s;
     steps++;
-    if(steps==9){
-        alert("tia");
+    if (steps == 9) {
+      alert(e.target.innerHTML);
+      document.location.reload(true);
+    } else if (isWin()){
+
     }
-}
+  }
+};/*
+function isWin(turn) {
+  return 
+      012 345 678
+      036 147 258
+        048 246
+}*/
