d6672895fd4e817757c103f10fd91beacfb74c09
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-06-19 19:43:52 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,31 +10,39 @@ s = "";
 turn = true;
 steps = 0;
 document.onclick = function(e) {
-  if (e.target.id=="turn") {
-    if (turns.innerHTML.includes(o.innerHTML)) {
-      s = o.innerHTML;
-      t = x.innerHTML;
-    } 
-    if (turns.innerHTML.includes(x.innerHTML)) {
-      s = x.innerHTML;
-      t = o.innerHTML;
-    }
-  }
+if (e.target.id == "turn") {
+      toggle();
+    turns.innerHTML = t;
+    } else 
   if (e.target.type == "submit" && e.target.innerHTML == "") {
     e.target.disabled = true;
     document.getElementById("turn").disabled = true;
     
+    toggle();
     turns.innerHTML = t;
     e.target.innerHTML = s;
     steps++;
     if (steps == 9) {
-      alert(e.target.innerHTML);
-      document.location.reload(true);
-    } else if (isWin()){
+      reload();
+      alert(e.target.innerHTML);    
+    }/* else if (isWin()){
 
-    }
+     }*/
   }
-};/*
+};
+function toggle(){
+  if (turns.innerHTML.includes(o.innerHTML)) {
+        s = o.innerHTML;
+        t = x.innerHTML;
+      } 
+      if (turns.innerHTML.includes(x.innerHTML)) {
+        s = x.innerHTML;
+        t = o.innerHTML;
+      }
+}
+function reload(){
+  document.location.reload(true);
+}/*
 function isWin(turn) {
   return 
       012 345 678
