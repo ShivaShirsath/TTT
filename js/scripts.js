c228274430dfa7295778eb133fe0f6af4eaa0867
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-06-19 23:09:37 +0530

    Winning condition added

--- js/scripts.js
+++ js/scripts.js
@@ -10,42 +10,59 @@ s = "";
 turn = true;
 steps = 0;
 document.onclick = function(e) {
-if (e.target.id == "turn") {
-      toggle();
-    turns.innerHTML = t;
-    } else 
-  if (e.target.type == "submit" && e.target.innerHTML == "") {
+  if (e.target.id == "turn") {
+    toggle();  
+  } else if (e.target.type == "submit" && e.target.innerHTML == "") {
     e.target.disabled = true;
     document.getElementById("turn").disabled = true;
-    
     toggle();
-    turns.innerHTML = t;
     e.target.innerHTML = s;
     steps++;
-    if (steps == 9) {
-      reload();
-      alert(e.target.innerHTML);    
-    }/* else if (isWin()){
-
-     }*/
+    checkWin();
   }
 };
+function checkWin(){
+  r = "";
+  if (isWin(s)){
+    r = s + " is Winner";
+  } else if (steps == 9) {
+    r = "Draw";
+  }
+  if (!r == "") {
+    alert(r);
+    reload(); 
+  } 
+}
 function toggle(){
   if (turns.innerHTML.includes(o.innerHTML)) {
-        s = o.innerHTML;
-        t = x.innerHTML;
-      } 
-      if (turns.innerHTML.includes(x.innerHTML)) {
-        s = x.innerHTML;
-        t = o.innerHTML;
-      }
+    s = o.innerHTML;
+    t = x.innerHTML;
+  } 
+  if (turns.innerHTML.includes(x.innerHTML)) {
+    s = x.innerHTML;
+    t = o.innerHTML;
+  }
+  turns.innerHTML = t;
 }
+
+function check(e0, e1, e2, s){
+  return document.getElementById(e0).innerHTML.includes(s) &&
+    document.getElementById(e1).innerHTML.includes(s) &&
+    document.getElementById(e2).innerHTML.includes(s);
+}
+
+function isWin(s) {
+  return check("0", "1", "2", s) || 
+         check("3", "4", "5", s) ||
+         check("6", "7", "8", s) ||
+         check("0", "3", "6", s) ||
+         check("1", "4", "7", s) ||
+         check("2", "5", "8", s) ||
+         check("0", "4", "8", s) ||
+         check("6", "4", "2", s)
+  ;
+}
+
 function reload(){
   document.location.reload(true);
-}/*
-function isWin(turn) {
-  return 
-      012 345 678
-      036 147 258
-        048 246
-}*/
+}
